1c94440d55ba448412c6599c47d123956ce91c9f
Thales-arias <thalesaraujodias@gmail.com>
2025-09-14 15:39:59 -0300

    01

--- 01_box_plot_logo.R
+++ 01_box_plot_logo.R
@@ -6,37 +6,34 @@ library(tidyverse)
 
 
 # Criar tibble
-dados <- tibble(valor = c(5, 5, 6, 6, 6, 7, 
-                          8, 9, 9, 9, 10, 11)
-                )
+dados_notas <- tibble(notas = c(0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 10))
 
 # Visualizar o banco
-print(dados)
+print(dados_notas)
 
 # Boxplot - com instruções de cada passo
 ggplot(
-  dados, # banco a ser utilizado
-  aes(x = "Quartis consultoria", y = valor)) + # atribuição dos eixos x e y
+  dados_notas, # banco a ser utilizado
+  aes(x = "", y = notas)) + # atribuição dos eixos x e y
   geom_boxplot( # escolha do tipo de gráfico
     width = 0.12, # largura da caixa
     colour = "#00ac86", # contorno da caixa
     fill = "white", # caixa com fundo branco
     outlier.colour = "#00ac86", # cor do outlier
     outlier.shape = 19, # formato do outlier
-    outlier.size = 3, # tamanho do outlier
-    coef = 0.65 # multiplicador do IQR para definir outliers
+    outlier.size = 3 # tamanho do outlier
   )
 
-# Boxplot - limpo
+# Boxplot
 ggplot(
-  dados,
-  aes(x = "Quartis consultoria", y = valor)) +
+  dados_notas,
+  aes(x = "Quartis consultoria", y = notas)) +
   geom_boxplot(
     width = 0.12,
     colour = "#00ac86",
     fill = "white",
     outlier.colour = "#00ac86",
     outlier.shape = 19,
-    outlier.size = 3,
-    coef = 0.65
+    outlier.size = 3
   )
+
