27cd1d2228cca89d20d49e363a4cfe3e9736430f
Thales-arias <thalesaraujodias@gmail.com>
2025-09-15 18:05:58 -0300

    01_ok

--- 01_box_plot_logo.R
+++ 01_box_plot_logo.R
@@ -4,22 +4,22 @@
 # Carregando o pacote
 library(tidyverse)
 
-
 # Criar tibble
 dados_notas <- tibble(notas = c(0, 0, 1.5, 2, 2, 3, 4, 4, 5, 6, 10))
 
 # Visualizar o banco
 print(dados_notas)
 
-# Boxplot - com instruções de cada passo
-ggplot(
-  dados_notas, # banco a ser utilizado
-  aes(x = "", y = notas)) + # atribuição dos eixos x e y
-  geom_boxplot( # escolha do tipo de gráfico
-    width = 0.12, # largura da caixa
-    colour = "#00ac86", # contorno da caixa
-    fill = "white", # caixa com fundo branco
-    outlier.colour = "#00ac86", # cor do outlier
-    outlier.shape = 19, # formato do outlier
-    outlier.size = 3 # tamanho do outlier
-  )
+# Boxplot
+boxplot(
+  dados_notas$notas, # banco$variável
+  col = "white", # cor da caixa
+  border = "#00ac86", # cor das bordas
+  pch = 19, # formato do outlier
+  outcol = "#00ac86", # cor do outlier
+  boxwex = 0.12, # largura da caixa
+  staplewex = 1, # largura dos bigodes
+  whisklty = 1, # estilo da linha dos bigodes
+  ylab = "Notas" # rótulo no eixo X
+)
+
